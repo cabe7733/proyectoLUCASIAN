44f790e7302cc9193d5f254d41d9ecf676666be4
Esteban RG <estebanramirezgrajales@gmail.com>
2019-12-29 18:36:29 -0500

    seccion planes

--- prueba/src/app/components/planes/planes.component.ts
+++ prueba/src/app/components/planes/planes.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-planes',
   templateUrl: './planes.component.html',
   styleUrls: ['./planes.component.scss']
 })
 export class PlanesComponent implements OnInit {
-
+  public play = faCheck;
   constructor() { }
 
   ngOnInit() {
