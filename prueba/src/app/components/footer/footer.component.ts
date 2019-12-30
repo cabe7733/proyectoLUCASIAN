18c6cb609cf0685ca154014bbf31a80e2885a0dc
Esteban RG <estebanramirezgrajales@gmail.com>
2019-12-29 23:35:36 -0500

    new pipes

--- prueba/src/app/components/footer/footer.component.ts
+++ prueba/src/app/components/footer/footer.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  today = Date.now();
   constructor() { }
 
   ngOnInit() {
+    console.log(this.today);
   }
 
 }
