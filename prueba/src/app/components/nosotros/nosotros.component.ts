0858e01234fe4816590bb8066303d1a7d17c3457
Esteban RG <estebanramirezgrajales@gmail.com>
2019-12-29 22:54:50 -0500

    nosotros

--- prueba/src/app/components/nosotros/nosotros.component.ts
+++ prueba/src/app/components/nosotros/nosotros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from 'src/app/services/service.service';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nosotros',
@@ -9,11 +10,49 @@ import { ServiceService } from 'src/app/services/service.service';
 export class NosotrosComponent implements OnInit {
 
   constructor(private servicio:ServiceService) { }
+  img =[
+    {
+      img: '../../assets/img/img3.jpg'
+    },
+    {
+      img: '../../assets/img/img2.jpg'
+    },
+    {
+      img: '../../assets/img/img1.jpg'
+    },
+  ];
+
+  img2 = [
+    {
+      img: '../../assets/img/foto1.PNG'
+    },
+    {
+      img: 'https://www.analquim.com/wp-content/uploads/2017/12/fondo-blanco-300x169.jpg'
+    },
+    {
+      img: '../../assets/img/foto2.PNG'
+    },
+  ];
+  public play = faPlay;
+  public nombre1;
+  public nombre2;
+  public nombre3;
+  public cards;
+  slides: any = [[]];
+  chunk(arr: any, chunkSize:any) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
 
   ngOnInit() {
     this.servicio.peticion().subscribe((data) => {
-      console.log(data);
+      this.cards = data;
+      this.slides = this.chunk(this.cards, 3);
     });
+
   }
 
 }
